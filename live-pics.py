387a51cdeaf3b2695327f4e5fe2d075fc8bb3144
Vinny <VinnySanchez87@gmail.com>
2018-02-11 14:27:19 -0500

    [ Live Pics ] Swap 'VideoPlayer' for 'Video'.

--- live-pics.py
+++ live-pics.py
@@ -4,7 +4,7 @@ kivy.require('1.2.0')
 from sys import argv
 from os.path import dirname, join
 from kivy.app import App
-from kivy.uix.videoplayer import VideoPlayer
+from kivy.uix.video import Video
 
 # check what formats are supported for your targeted devices
 # for example try h264 video and acc audo for android using an mp4
@@ -14,12 +14,18 @@ from kivy.uix.videoplayer import VideoPlayer
 class VideoPlayerApp(App):
 
     def build(self):
+        
         if len(argv) > 1:
             filename = argv[1]
         else:
             curdir = dirname(__file__)
             filename = join(curdir, 'cityCC0.mpg')
-        return VideoPlayer(source=filename, state='play')
+        
+        video = Video(source=filename)
+        video.state = 'play'
+        video.options = {'eos': 'loop'}
+        video.allow_stretch=True
+        return video
 
 
 if __name__ == '__main__':
