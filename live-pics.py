3711a228fcee928c2335aed597c2d1cf01c74124
Vinny <VinnySanchez87@gmail.com>
2018-03-10 13:13:47 -0500

    Add message immediately.

--- live-pics.py
+++ live-pics.py
@@ -6,6 +6,7 @@ from os.path import dirname, join
 from kivy.app import App
 from kivy.uix.video import Video
 from kivy.core.window import Window
+from kivy.uix.label import Label
 
 # check what formats are supported for your targeted devices
 # for example try h264 video and acc audo for android using an mp4
@@ -30,12 +31,19 @@ class VideoPlayerApp(App):
 
         return video
 
+    def create_label(self):
+        return Label(text="Broooooooklyyyyyyyyn!!!")
+
     def configure_window(self):
         Window.borderless = True
         Window.rotation += 270
 
     def get_root_widget(self):
-        return self.create_video(self.get_filename())
+        video = self.create_video(self.get_filename())
+
+        video.add_widget(self.create_label())
+
+        return video
 
     def build(self):
         self.configure_window()
