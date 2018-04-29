a81e552c5c20cd03f1051a2878d49f1b654fa2cc
Vinny <VinnySanchez87@gmail.com>
2018-04-29 13:53:33 -0400

    [ LiveDisplay ] Refactor Raspberry Pi-oriented changes.

--- LiveDisplay.py
+++ LiveDisplay.py
@@ -7,34 +7,26 @@ class LiveDisplay(GridLayout):
 
     def __init__(self, videos, **kwargs):
         super(LiveDisplay, self).__init__(**kwargs)
-        self.pictures = Playlist(videos)
-        self.current = LivePicture(self.pictures.get_current())
+        self.playlist = Playlist(videos)
+        self.picture = LivePicture(self.playlist.get_current())
 
-    def show_live_picture(self, live_picture):
+    def show_live_picture(self, source):
         if len(self.children) == 0:
-            self.add_widget(self.current)
-            # self.add_widget(live_picture)
+            self.add_widget(self.picture)
 
-        if self.is_showing_picture(live_picture):
+        if self.is_showing_picture(source):
             return
 
-        # if self.current.source != live_picture.source:
-        if self.current.source != live_picture:
-            self.swap_picture(live_picture)
+        if self.picture.source != source:
+            self.swap_picture(source)
 
-        self.current.play()
+        self.picture.play()
 
     def is_showing_picture(self, picture):
-        return self.current and self.current.source == picture and self.current.state == 'play' and self.current.is_showing()
-        # return self.current and self.current == picture.source and self.current.state == 'play' and self.current.is_showing()
+        return self.picture and self.picture.source == picture and self.picture.state == 'play' and self.picture.is_showing()
 
-    def swap_picture(self, picture):
-        print("self.current:", self.current)
-        self.current.source = picture
-        # self.current.unload()
-        # self.remove_widget(self.current)
-        # self.add_widget(picture)
-        # self.current = picture
+    def swap_picture(self, source):
+        self.picture.source = source
 
     # Note: In order to receive touch events, need to add the following lines to
     # Kivy's 'config.ini' file:
@@ -48,11 +40,10 @@ class LiveDisplay(GridLayout):
     def on_touch_down(self, touch):
         print("touch:", touch)
         navigation_demarcation_x = 0.5
-        picture = self.pictures.previous() if touch.sx < navigation_demarcation_x else self.pictures.next()
-        # self.show_live_picture(LivePicture(picture))
+        picture = self.playlist.previous() if touch.sx < navigation_demarcation_x else self.playlist.next()
         self.show_live_picture(picture)
         return True
 
     def start(self):
-        self.show_live_picture(self.current.source)
+        self.show_live_picture(self.picture.source)
         return self
